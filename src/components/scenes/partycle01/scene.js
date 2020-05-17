c3f2233d516b3f59e969e1aaad086d8cec836219
Michael Hazani <123mickey@gmail.com>
2020-05-17 18:17:00 +0300

    update partyclescene

--- src/components/scenes/partycle01/scene.js
+++ src/components/scenes/partycle01/scene.js
@@ -1,37 +1,9 @@
 import { Scene, Mesh, SphereGeometry, MeshNormalMaterial, Vector3, Group, PerspectiveCamera } from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { PointerLockControls } from "three/examples/jsm/controls/PointerLockControls"
 import { EditorCamera } from "../../engine/util/editorCamera"
 import { Physics } from '../../engine/physics';
 import { renderer } from '../../engine/renderer';
 import { ctrlArr } from '../../engine/xrinput';
 import { PeerConnection } from '../../engine/networking/PeerConnection'
-
-
-// screenCamera.position.set(0, 0, 5);
-// const OC = new OrbitControls(screenCamera, renderer.domElement);
-
-// const controls = new PointerLockControls(screenCamera, renderer.domElement);
-// document.addEventListener('click', () => { controls.lock(); });
-// const onKeyDown = function (event)
-// {
-//     switch (event.keyCode)
-//     {
-//         case 87: // w
-//             controls.moveForward(.25)
-//             break;
-//         case 65: // a
-//             controls.moveRight(-.25)
-//             break;
-//         case 83: // s
-//             controls.moveForward(-.25)
-//             break;
-//         case 68: // d
-//             controls.moveRight(.25)
-//             break;
-//     }
-// };
-// document.addEventListener('keydown', onKeyDown, false);
 
 const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const editorCamera = new EditorCamera(camera, renderer.domElement);
@@ -42,11 +14,8 @@ Physics.enableDebugger(scene);
 
 scene.initGame = () =>
 {
-
-
     //clean up scene and physics
     // Physics.resetScene();
-
     scene.traverse(e =>
     {
         scene.remove(e);
