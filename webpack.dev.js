9f64309c72f795eb77d3d95932fa7db9fbd5cf11
Michael Hazani <123mickey@gmail.com>
2020-06-15 17:22:48 +0300

    disable host check for https tunneling

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,6 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
-const os = require('os');
-
-const getLocalExternalIp = () => [].concat.apply([], Object.values(os.networkInterfaces()))
-    .filter(details => details.family === 'IPv4' && !details.internal)
-    .pop().address.toString();
 
 const APP_DIR = path.resolve(__dirname, 'src/');
 
@@ -14,9 +9,9 @@ module.exports = merge(common, {
     devtool: "eval-source-map",
     devServer: {
         contentBase: APP_DIR,
-        // host: getLocalExternalIp(),
-        // public: getLocalExternalIp(),
-        // disableHostCheck: true,
+        hot: true,
+        open: true,
+        disableHostCheck: true,
         port: 1234
     }
 });
