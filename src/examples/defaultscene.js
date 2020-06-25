bf1a9b1674b8bc67d315580dc5f182e9f56f03e5
Michael Hazani <123mickey@gmail.com>
2020-06-25 13:51:37 +0300

    lit default scene

--- src/examples/defaultscene.js
+++ src/examples/defaultscene.js
@@ -1,40 +1,45 @@
 // default scene loaded in src/engine/engine.js
-
 import {
   Scene,
   TorusBufferGeometry,
-  MeshNormalMaterial,
+  DirectionalLight,
+  AmbientLight,
+  HemisphereLight,
   Mesh,
   Vector3,
+  MeshStandardMaterial,
+  Color,
 } from "three";
 const scene = new Scene();
 
-scene.init = () => {
-  scene.traverse(e => {
-    scene.remove(e);
-  });
+const rings = [
+  [new Vector3(1, 0, 1), new Color(0xff0000), 0.2],
+  [new Vector3(1, -1, 0), new Color(0x00ff00), 0.15],
+  [new Vector3(0, 1, 0), new Color(0x0000ff), 0.1],
+];
 
-  const rots = [
-    new Vector3(-1, 0, 0),
-    new Vector3(1, 1, 0),
-    new Vector3(0, -1, 0),
-  ];
-  for (var i = 0; i < 3; i++) {
-    const placeholder = new Mesh(
-      new TorusBufferGeometry(1, 0.05, 16, 32),
-      new MeshNormalMaterial({ wireframe: true })
-    );
-    placeholder.position.z -= 5;
-    placeholder.scale.set(1 - i * 0.33, 1 - i * 0.33, 1 - i * 0.33);
-    const axis = new Vector3();
-    axis.copy(rots[i]);
-    placeholder.Update = () => {
-      placeholder.rotateOnAxis(axis, 0.007);
-    };
-    scene.add(placeholder);
-  }
-};
+rings.forEach((entry, i) => {
+  const ring = new Mesh(
+    new TorusBufferGeometry(1, 0.065, 64, 64),
+    new MeshStandardMaterial({
+      metalness: 0.5,
+      roughness: 0.5,
+      color: entry[1],
+    })
+  );
+  ring.position.z -= 1;
+  ring.scale.set(entry[2], entry[2], entry[2]);
 
-scene.init();
+  const axis = new Vector3();
+  axis.copy(entry[0]);
+  ring.Update = () => {
+    ring.rotateOnAxis(axis, 0.0033 * (i + 1));
+  };
+  scene.add(ring);
+});
+
+const light = new DirectionalLight(0xffffff, 3.5);
+light.position.set(0, 13, 3);
+scene.add(light);
 
 export { scene };
