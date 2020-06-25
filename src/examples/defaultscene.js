93b93006c9d4c6c55f44e24a351f9979df999825
Michael Hazani <123mickey@gmail.com>
2020-06-26 00:32:43 +0300

    integrate PR feedback

--- src/examples/defaultscene.js
+++ src/examples/defaultscene.js
@@ -10,28 +10,26 @@ import {
 } from "three";
 const scene = new Scene();
 
-const rings = [
-  [new Vector3(1, 0, 1), new Color(0xff0000), 0.2],
-  [new Vector3(1, -1, 0), new Color(0x00ff00), 0.15],
-  [new Vector3(0, 1, 0), new Color(0x0000ff), 0.1],
+const ringsData = [
+  { axis: new Vector3(1, 0, 1), color: new Color(0xff0000), scale: 0.2 },
+  { axis: new Vector3(1, -1, 0), color: new Color(0x00ff00), scale: 0.15 },
+  { axis: new Vector3(0, 1, 0), color: new Color(0x0000ff), scale: 0.1 },
 ];
 
-rings.forEach((entry, i) => {
+ringsData.forEach((ringData, i) => {
   const ring = new Mesh(
     new TorusBufferGeometry(1, 0.065, 64, 64),
     new MeshStandardMaterial({
       metalness: 0.5,
       roughness: 0.5,
-      color: entry[1],
+      color: ringData.color,
     })
   );
   ring.position.z -= 1;
-  ring.scale.set(entry[2], entry[2], entry[2]);
+  ring.scale.set(ringData.scale, ringData.scale, ringData.scale);
 
-  const axis = new Vector3();
-  axis.copy(entry[0]);
   ring.Update = () => {
-    ring.rotateOnAxis(axis, 0.0033 * (i + 1));
+    ring.rotateOnAxis(ringData.axis, 0.0033 * (i + 1));
   };
   scene.add(ring);
 });
