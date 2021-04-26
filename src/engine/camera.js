095ca9eabaab9a6adcc1ac1b325b72b010e61383
Tim Carlson <timgcarlson@gmail.com>
2021-04-26 11:57:11 -0700

    Update the camera position to give HMD position/rotation data

--- src/engine/camera.js
+++ src/engine/camera.js
@@ -1,10 +1,50 @@
-import { PerspectiveCamera } from "three";
+import { log, Matrix4, PerspectiveCamera, Quaternion, Vector3 } from "three";
+import State from "./state";
 
-const Camera = new PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  10000
-);
+class TrackingCamera extends PerspectiveCamera {
+  constructor() {
+    super(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+
+    this.xrReferenceSpace = null;
+
+    State.eventHandler.addEventListener("xrsessionstarted", session => {
+      this.requestReferenceSpace(session);
+      session.requestAnimationFrame(this.onXRAnimationFrame);
+    });
+
+    State.eventHandler.addEventListener("xrsessionended", () => {
+      this.xrReferenceSpace = null;
+    });
+  }
+
+  requestReferenceSpace = session => {
+    session.requestReferenceSpace("local").then(refSpace => {
+      this.xrReferenceSpace = refSpace;
+      ``;
+    });
+  };
+
+  onXRAnimationFrame = (time, xrFrame) => {
+    State.currentSession.requestAnimationFrame(this.onXRAnimationFrame);
+    this.updateCameraPosition(xrFrame);
+  };
+
+  updateCameraPosition = xrFrame => {
+    if (this.xrReferenceSpace == null) return;
+    let viewerPose = xrFrame.getViewerPose(this.xrReferenceSpace);
+    // The transform is an object of type XRRigidTransform https://developer.mozilla.org/en-US/docs/Web/API/XRRigidTransform
+    const { position, orientation } = viewerPose.transform;
+
+    this.position.set(position.x, position.y, position.z);
+    this.quaternion.set(
+      orientation.x,
+      orientation.y,
+      orientation.z,
+      orientation.w
+    );
+  };
+}
+
+const Camera = new TrackingCamera();
 
 export default Camera;
