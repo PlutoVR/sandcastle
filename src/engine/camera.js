1f3bd80ee77c9ddf7c0a89a4e231ef150ea0e368
timgcarlson <timgcarlson@gmail.com>
2021-04-27 10:42:09 -0700

    Decompose the camera matrix in the Update function

--- src/engine/camera.js
+++ src/engine/camera.js
@@ -1,82 +1,15 @@
-import { PerspectiveCamera, Vector3, Quaternion } from "three";
+import { PerspectiveCamera } from "three";
 import State from "./state";
 
 class TrackingCamera extends PerspectiveCamera {
   constructor() {
     super(75, window.innerWidth / window.innerHeight, 0.1, 10000);
-
-    this.xrReferenceSpace = null;
-
-    this.startTime = 0;
-    this.counter = 0;
-    this.totalMS = 0;
-    this.FRAMES_TO_COUNT = 300;
-    this.doOnce = false;
-
-    State.eventHandler.addEventListener("xrsessionstarted", session => {
-      this.requestReferenceSpace(session);
-      session.requestAnimationFrame(this.onXRAnimationFrame);
-    });
-
-    State.eventHandler.addEventListener("xrsessionended", () => {
-      this.xrReferenceSpace = null;
-      this.counter = 0;
-      this.doOnce = false;
-    });
   }
 
-  requestReferenceSpace = session => {
-    session.requestReferenceSpace("local").then(refSpace => {
-      this.xrReferenceSpace = refSpace;
-      ``;
-    });
-  };
-
-  onXRAnimationFrame = (time, xrFrame) => {
-    State.currentSession.requestAnimationFrame(this.onXRAnimationFrame);
-    this.updateCameraPosition(xrFrame);
-  };
-
-  updateCameraPosition = xrFrame => {
-    if (this.xrReferenceSpace == null) return;
-
-    // Start test
-    this.startTime = performance.now();
-
-    // -------------------
-    // // * xrReferenceSpace/getViewerPose method (A): Michael timed ~0.04ms average *
-    // let viewerPose = xrFrame.getViewerPose(this.xrReferenceSpace);
-    // // The transform is an object of type XRRigidTransform https://developer.mozilla.org/en-US/docs/Web/API/XRRigidTransform
-    // const { position, orientation } = viewerPose.transform;
-
-    // this.position.set(position.x, position.y, position.z);
-    // this.quaternion.set(
-    //   orientation.x,
-    //   orientation.y,
-    //   orientation.z,
-    //   orientation.w
-    // );
-    // -------------------
-
-    // -------------------
-    // * matrix decomp method (B): Michael timed ~0.016ms average *
+  Update() {
+    if (!State.isXRSession) return;
     this.matrixWorld.decompose(this.position, this.quaternion, this.scale);
-    // -------------------
-
-    // end test
-    if (this.counter <= this.FRAMES_TO_COUNT) {
-      this.totalMS += performance.now() - this.startTime;
-      this.counter++;
-    } else {
-      if (!this.doOnce) {
-        console.warn(`average time: ${this.totalMS / this.FRAMES_TO_COUNT}`);
-        this.doOnce = true;
-        this.totalMS = 0;
-        this.startTime = 0;
-        this.counter = 0;
-      }
-    }
-  };
+  }
 }
 
 const Camera = new TrackingCamera();
